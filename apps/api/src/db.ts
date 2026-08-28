6e5941c44b4a965704e8d24d24bde2525c9a5442
sj2318 <seunjun2318@seagrs.com>
2026-08-28 10:12:20 +0900

    fix: support render environment variables

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,11 +1,19 @@
+import { existsSync } from "node:fs";
 import { loadEnvFile } from "node:process";
 import { Pool } from "pg";
-loadEnvFile();
 
-export const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-});
+if (existsSync(".env")) {
+  loadEnvFile();
+}
+
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+    })
+  : new Pool({
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    });
